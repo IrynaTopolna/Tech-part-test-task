0f6062b6ba426d09b5af8ead484142d098cde979
IrynaTopolna <itopolna2808@gmail.com>
2023-04-21 14:09:58 +0300

    Adds app base

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { GlobalStyles } from "./GlobalStyles";
+import Layout from "./Layout/Layout";
+
+export default function App() {
+  return (
+    <Layout>
+      <p>Hello</p>
+
+      <GlobalStyles />
+    </Layout>
+  );
+}
